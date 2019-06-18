8c7eedde565d14e8eae2acfbe2e7c4cc6540338b
unknown <koyasairammanohar@gmail.com>
2019-06-18 22:34:06 +0530

    Added some gradients and some content in home

--- app/src/main/java/com/example/cyberaegisapp/MainActivity.java
+++ app/src/main/java/com/example/cyberaegisapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
+import android.view.InflateException;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
